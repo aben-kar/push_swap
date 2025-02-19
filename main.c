f064fb32531b54b387d3d94f69d461f3a897891d
Achraf Ben Kardoud <acben-ka@c3r2p8.1337.ma>
2025-02-19 23:03:33 +0100

    po

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: acben-ka <acben-ka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 14:08:59 by acben-ka          #+#    #+#             */
-/*   Updated: 2025/02/18 22:38:27 by acben-ka         ###   ########.fr       */
+/*   Updated: 2025/02/19 21:21:02 by acben-ka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ int main(int ac, char **av)
         }
         
 
-        // printlist(a);
+        printlist(a);
         // printlist(b);
 
         free_list(&a);
--- sort_five_number.c
+++ sort_five_number.c
@@ -59,42 +59,90 @@ void four_nbr(t_Node **head_a, t_Node **head_b)
         pa(head_a, head_b);
 }
 
+// void five_nbr(t_Node **head_a, t_Node **head_b)
+// {
+//     if (is_sorted(*head_a) == 1)
+// 		return;
+//     pb(head_a, head_b);
+// 	four_nbr(head_a, head_b);
+
+//     if (((*head_b)->data > (*head_a)->data) && ((*head_b)->data > (*head_a)->next->next->next->data))
+//     {
+//         pa(head_a, head_b);
+//         ra(head_a);
+//     }
+//     if (((*head_b)->data > (*head_a)->data) && ((*head_b)->data < (*head_a)->next->data))
+//     {
+//         ra(head_a);
+//         pa(head_a, head_b);
+//         rra(head_a);
+//     }
+//     if (((*head_b)->data > (*head_a)->next->data) && ((*head_b)->data < (*head_a)->next->next->data))
+//     {
+//         ra(head_a);
+//         ra(head_a);
+//         pa(head_a, head_b);
+//         rra(head_a);
+//         rra(head_a);
+//     }
+//     if (((*head_b)->data > (*head_a)->next->next->data) && ((*head_b)->data < (*head_a)->next->next->next->data))
+//     {
+//         ra(head_a);
+//         ra(head_a);
+//         ra(head_a);
+//         pa(head_a, head_b);
+//         rra(head_a);
+//         rra(head_a);
+//         rra(head_a);
+//     }
+//     if (((*head_b)->data < (*head_a)->data) && ((*head_b)->data < (*head_a)->next->next->next->data))
+//         pa(head_a, head_b);
+// }
+
+t_Node *find_min_node(t_Node **head)
+{
+    if (!*head)
+        return 0;  // Empty list
+
+    t_Node *current;
+    t_Node *min_value;
+
+    min_value = *head;
+    current = (*head)->next;
+
+    while (current != NULL)
+    {
+        if (current->data < min_value->data)
+            min_value = current;
+        current = current->next;
+    }
+    return min_value;  // Return the minimum value
+}
+
+
 void five_nbr(t_Node **head_a, t_Node **head_b)
 {
     if (is_sorted(*head_a) == 1)
 		return;
-    pb(head_a, head_b);
-	four_nbr(head_a, head_b);
+    t_Node *min = find_min_node(head_a);
 
-    if (((*head_b)->data > (*head_a)->data) && ((*head_b)->data > (*head_a)->next->next->next->data))
+
+    // if (is_sorted(*head_b) == 1)
+    // {
+    //     pa(head_a, head_b);
+    //     pa(head_a, head_b);
+    // }
+
+    if ((*head_b)->data > (*head_b)->next->data)
     {
         pa(head_a, head_b);
-        ra(head_a);
+        pa(head_a, head_b);
     }
-    if (((*head_b)->data > (*head_a)->data) && ((*head_b)->data < (*head_a)->next->data))
+    
+    if ((*head_b)->data < (*head_b)->next->data)
     {
-        ra(head_a);
+        sb(head_b);
         pa(head_a, head_b);
-        rra(head_a);
+        pa(head_a, head_b);
     }
-    if (((*head_b)->data > (*head_a)->next->data) && ((*head_b)->data < (*head_a)->next->next->data))
-    {
-        ra(head_a);
-        ra(head_a);
-        pa(head_a, head_b);
-        rra(head_a);
-        rra(head_a);
-    }
-    if (((*head_b)->data > (*head_a)->next->next->data) && ((*head_b)->data < (*head_a)->next->next->next->data))
-    {
-        ra(head_a);
-        ra(head_a);
-        ra(head_a);
-        pa(head_a, head_b);
-        rra(head_a);
-        rra(head_a);
-        rra(head_a);
-    }
-    if (((*head_b)->data < (*head_a)->data) && ((*head_b)->data < (*head_a)->next->next->next->data))
-        pa(head_a, head_b);
 }
