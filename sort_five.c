bee1450cd1abe5370c5fbb7cc908918225240e6b
Achraf Ben Kardoud <acben-ka@e1r12p2.1337.ma>
2025-02-24 22:30:18 +0000

    ok

--- sort_five.c
+++ sort_five.c
@@ -6,66 +6,59 @@
 /*   By: acben-ka <acben-ka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 19:21:00 by acben-ka          #+#    #+#             */
-/*   Updated: 2025/02/24 21:56:23 by acben-ka         ###   ########.fr       */
+/*   Updated: 2025/02/24 22:29:35 by acben-ka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void two_nbr(t_Node **head_a)
+void	two_nbr(t_Node **head_a)
 {
-    if ((*head_a)->data > (*head_a)->next->data)
-    {
-        sa(head_a);
-    }
+	if ((*head_a)->data > (*head_a)->next->data)
+		sa(head_a);
 }
 
-
-void three_nbr(t_Node **head_a)
+void	three_nbr(t_Node **head_a)
 {
-    if (((*head_a)->data > (*head_a)->next->data)
-        && ((*head_a)->data > (*head_a)->next->next->data))
-        ra(head_a);
-    if (((*head_a)->data > (*head_a)->next->data)
-        && ((*head_a)->data < (*head_a)->next->next->data))
-        sa(head_a);
-    if (((*head_a)->data < (*head_a)->next->data)
-        && ((*head_a)->data < (*head_a)->next->next->data)
-        && ((*head_a)->next->data > (*head_a)->next->next->data))
-        rra(head_a);
-    if (((*head_a)->data < (*head_a)->next->data)
-        && ((*head_a)->data > (*head_a)->next->next->data))
-        rra(head_a);
-    if ((*head_a)->data > (*head_a)->next->data)
-        sa(head_a);
+	if (((*head_a)->data > (*head_a)->next->data)
+		&& ((*head_a)->data > (*head_a)->next->next->data))
+		ra(head_a);
+	if (((*head_a)->data > (*head_a)->next->data)
+		&& ((*head_a)->data < (*head_a)->next->next->data))
+		sa(head_a);
+	if (((*head_a)->data < (*head_a)->next->data)
+		&& ((*head_a)->data < (*head_a)->next->next->data)
+		&& ((*head_a)->next->data > (*head_a)->next->next->data))
+		rra(head_a);
+	if (((*head_a)->data < (*head_a)->next->data)
+		&& ((*head_a)->data > (*head_a)->next->next->data))
+		rra(head_a);
+	if ((*head_a)->data > (*head_a)->next->data)
+		sa(head_a);
 }
 
-void four_nbr(t_Node **head_a, t_Node **head_b)
+void	four_nbr(t_Node **head_a, t_Node **head_b)
 {
-    if (is_sorted(*head_a) == 1)
-        return ;
-    
-    int min_pos;
-    min_pos = find_min_position(*head_a);
-    move_min_to_top(head_a, min_pos);
-    pb(head_a, head_b);
-    
-    three_nbr(head_a);
-    
-    pa(head_a, head_b);
+	int	min_pos;
+
+	if (is_sorted(*head_a) == 1)
+		return ;
+	min_pos = find_min_position(*head_a);
+	move_min_to_top(head_a, min_pos);
+	pb(head_a, head_b);
+	three_nbr(head_a);
+	pa(head_a, head_b);
 }
 
-void five_nbr(t_Node **head_a, t_Node **head_b)
+void	five_nbr(t_Node **head_a, t_Node **head_b)
 {
-    if (is_sorted(*head_a) == 1)
-        return;
+	int	min_pos;
 
-    int min_pos;
-    min_pos = find_min_position(*head_a);
-    move_min_to_top1(head_a, min_pos);
-    pb(head_a, head_b);
-
-    four_nbr(head_a, head_b);
-
-    pa(head_a, head_b);
+	if (is_sorted(*head_a) == 1)
+		return ;
+	min_pos = find_min_position(*head_a);
+	move_min_to_top1(head_a, min_pos);
+	pb(head_a, head_b);
+	four_nbr(head_a, head_b);
+	pa(head_a, head_b);
 }
--- sort_utils.c
+++ sort_utils.c
@@ -6,64 +6,67 @@
 /*   By: acben-ka <acben-ka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 19:21:10 by acben-ka          #+#    #+#             */
-/*   Updated: 2025/02/21 13:56:48 by acben-ka         ###   ########.fr       */
+/*   Updated: 2025/02/24 22:29:45 by acben-ka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int find_min_position(t_Node *head)
+int	find_min_position(t_Node *head)
 {
-    int min = head->data;
-    int pos = 0;
-    int i = 0;
+	int	min;
+	int	pos;
+	int	i;
 
-    while (head)
-    {
-        if (head->data < min)
-        {
-            min = head->data;
-            pos = i;
-        }
-        head = head->next;
-        i++;
-    }
-    return pos;
+	min = head->data;
+	pos = 0;
+	i = 0;
+	while (head)
+	{
+		if (head->data < min)
+		{
+			min = head->data;
+			pos = i;
+		}
+		head = head->next;
+		i++;
+	}
+	return (pos);
 }
 
-void move_min_to_top(t_Node **head, int min_pos)
+void	move_min_to_top(t_Node **head, int min_pos)
 {
-    if (min_pos == 0)
-        return ;
-    if (min_pos == 1)
-        sa(head);
-    else if (min_pos == 2)
-    {
-        ra(head);
-        sa(head);
-    }
-    else if (min_pos == 3)
-        rra(head);
+	if (min_pos == 0)
+		return ;
+	if (min_pos == 1)
+		sa(head);
+	else if (min_pos == 2)
+	{
+		ra(head);
+		sa(head);
+	}
+	else if (min_pos == 3)
+		rra(head);
 }
 
-void move_min_to_top1(t_Node **head, int min_pos)
+void	move_min_to_top1(t_Node **head, int min_pos)
 {
-    if (min_pos == 0)
-        return ;
-    if (min_pos == 1)
-        sa(head);
-    else if (min_pos == 2)
-    {
-        ra(head);
-        sa(head);
-    }
-    else if (min_pos == 3)
-    {
-        rra(head);
-        rra(head);
-    }
-    else if (min_pos == 4)
-        rra(head);
+	if (min_pos == 0)
+		return ;
+	if (min_pos == 1)
+		sa(head);
+	else if (min_pos == 2)
+	{
+		ra(head);
+		sa(head);
+	}
+	else if (min_pos == 3)
+	{
+		rra(head);
+		rra(head);
+	}
+	else if (min_pos == 4)
+		rra(head);
 }
 
 int	is_sorted(t_Node *head)
